0b0c734136d41bf6c64827b74ddadfcea65ab4f1
Kuldeep Soni <49443497+SwapeeSoni@users.noreply.github.com>
2020-02-09 00:06:10 +0530

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,5 +1,4 @@
 <?php
-//establishing connection with database.
 
 mysql_connect('localhost','root','') or die('Cannot connect to server');
 mysql_select_db('attsystem') or die ('Cannot found database');
